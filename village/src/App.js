0a74cfa0c0b09c8079f12d507e1837a8a744269e
alyssa-hatfield <47366310+xCthaeh@users.noreply.github.com>
2019-05-10 12:11:01 -0400

    update/delete functionality

--- village/src/App.js
+++ village/src/App.js
@@ -5,6 +5,7 @@ import Smurfs from "./components/Smurfs";
 import axios from "axios";
 import { Route } from "react-router-dom";
 import NavBar from "./components/NavBar.js";
+import SmurfPage from "./components/SmurfPage";
 
 class App extends Component {
   constructor(props) {
@@ -19,9 +20,19 @@ class App extends Component {
   fetchSmurfs = () => {
     axios
       .get("http://localhost:3333/smurfs")
-      .then(res => {
-        this.setState({ smurfs: res.data });
-      })
+      .then(res => this.setState({ smurfs: res.data }))
+      .catch(err => console.log(err));
+  };
+  deleteSmurf = id => {
+    axios
+      .delete(`http://localhost:3333/smurfs/${id}`)
+      .then(res => this.setState({ smurfs: res.data }))
+      .catch(err => console.log(err));
+  };
+  updateSmurf = (id, info) => {
+    axios
+      .put(`http://localhost:3333/smurfs/${id}`, info)
+      .then(res => this.setState({ smurfs: res.data }))
       .catch(err => console.log(err));
   };
   render() {
@@ -29,11 +40,24 @@ class App extends Component {
       <div className="App">
         <Route path="/" component={NavBar} />
         <Route
+          exact
+          path="/"
+          render={props => (
+            <Smurfs
+              {...props}
+              smurfs={this.state.smurfs}
+              deleteSmurf={this.deleteSmurf}
+              updateSmurf={this.updateSmurf}
+            />
+          )}
+        />
+        <Route
           path="/smurf-form"
           render={props => (
             <SmurfForm {...props} fetchSmurfs={this.fetchSmurfs} />
           )}
         />
+        <Route path="/smurf/:id" component={SmurfPage} />
       </div>
     );
   }
